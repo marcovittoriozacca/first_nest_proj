c33fd080bb51555faf125bd9d95b072b1af739fa
marcovittoriozacca <zaccamarco01@gmail.com>
2024-09-04 15:56:46 +0200

    feat: auth provider and controller routes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
-@Controller({})
-export class AuthController {}
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Get('login')
+  login() {
+    return this.authService.login();
+  }
+
+  @Post('signup')
+  signup() {
+    return this.authService.signup();
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable({})
-export class AuthService {}
+export class AuthService {
+  login() {
+    return { message: "You're now logged in" };
+  }
+
+  signup() {
+    return { message: 'You signed up correctly' };
+  }
+}
