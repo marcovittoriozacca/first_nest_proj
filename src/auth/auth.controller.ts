65e96c30f5b7e234bbdcb5149db774f4d4572b51
marcovittoriozacca <zaccamarco01@gmail.com>
2024-09-04 16:49:46 +0200

    prisma module and service + auth dto

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,21 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  @Get('login')
+  //login - POST - route at /auth/login
+  @Post('login')
   login() {
     return this.authService.login();
   }
 
+  //signup - POST - route at /auth/signup
   @Post('signup')
-  signup() {
-    return this.authService.signup();
+  signup(@Body() dto: AuthDto) {
+    console.log(dto);
+    return this.authService.signup(dto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable({})
 export class AuthService {
+  constructor(private prismaService: PrismaService) {}
+
   login() {
     return { message: "You're now logged in" };
   }
 
-  signup() {
+  signup(dto: any) {
     return { message: 'You signed up correctly' };
   }
 }
