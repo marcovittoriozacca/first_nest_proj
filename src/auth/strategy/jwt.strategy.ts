5448861fd929a5d6f0233a59d527cedd416737bc
marcovittoriozacca <zaccamarco01@gmail.com>
2024-09-05 08:16:18 +0200

    feat: jwt strategy to decod the access token

---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private config: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: config.get('JWT_SECRET'),
+    });
+  }
+}
